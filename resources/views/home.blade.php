70582da86969969c8a288726b7cf7538e70ee427
davide sbalzer <sbalzerdavide@hotmail.it>
2021-01-07 17:26:24 +0100

    main content

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,12 +8,51 @@
         <div class="container">
             <div class="comics-list">
                 @foreach ($comics as $comic)
-                    <div class="box">
+                    <a class="box" href="">
                         <img src="{{ $comic['image'] }}" alt="">
-                    </div>
-
+                        <h3>{{ $comic['title'] }}</h3>
+                    </a>
                 @endforeach
             </div>
+        </div>
+        <button class="btn btn-primary">
+            learn more
+        </button>
+    </section>
+    <section class="icon-section">
+        <div class="container">
+            <ul class="icon">
+                <li>
+                    <img src="{{ asset('images/digital-comics.png') }}" alt="">
+                    <p>
+                        digital comics
+                    </p>
+                </li>
+                <li>
+                    <img src="{{ asset('images/merchandise.png') }}" alt="">
+                    <p>
+                        dc merchandise
+                    </p>
+                </li>
+                <li>
+                    <img src="{{ asset('images/subscriptions.png') }}" alt="">
+                    <p>
+                        subscription
+                    </p>
+                </li>
+                <li>
+                    <img src="{{ asset('images/shop-locator.png') }}" alt="">
+                    <p>
+                        comic shop locator
+                    </p>
+                </li>
+                <li>
+                    <img src="{{ asset('images/power-visa.svg') }}" alt="">
+                    <p>
+                        dc power visa
+                    </p>
+                </li>
+            </ul>
         </div>
     </section>
 @endsection
